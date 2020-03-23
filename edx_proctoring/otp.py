fbf8b2b083104023ba2545d032146e5f7bd65787
MasterTos <MasterTos@yahoo.com>
2020-03-23 11:10:56 +0700

    import user

--- edx_proctoring/otp.py
+++ edx_proctoring/otp.py
@@ -6,6 +6,7 @@ import pytz
 from django.core.mail.message import EmailMessage
 from django.template import loader
 from edx_proctoring.models import ProctoredExamStudentOTP
+from django.contrib.auth.models import User
 # from edx_proctoring.views import ProctoredAPIView
 
 def update_otp_status(otp, status):
